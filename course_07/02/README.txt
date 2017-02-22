875290a386cb9be3dc0226160e916a5132067271
zhushh <zsh5318@gmail.com>
2017-02-22 18:06:32 +0800

    upload how to use client API project

---
+++ course_07/02/README.txt
@@ -0,0 +1,14 @@
+How to run?
+----
+
+Exec the following command in the root of project
+
+$ mvn -Pserver
+
+then open the address: http://localhost:9000/customerservice/customers/123
+
+
+Reference blog
+----
+
+https://github.com/zhushh/Note/blob/master/webService%E5%AD%A6%E4%B9%A0/webService_02_web_service%E6%A1%86%E6%9E%B6CXF%E5%AD%A6%E4%B9%A0.md
---
+++ course_07/02/src/main/java/demo/jaxrs/client/MyClient.java
@@ -0,0 +1,76 @@
+package demo.jaxrs.client;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Configurable;
+import javax.ws.rs.core.MediaType;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.Entity;
+
+public final class MyClient {
+
+    private MyClient() {
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://localhost:9000/customerservice/");
+        WebTarget customers = webTarget.path("customers");
+        WebTarget orders = webTarget.path("orders");
+
+        // // Sent HTTP GET request to query customer info
+        System.out.println("Sent HTTP GET request to query customer info");
+        System.out.println(customers.path("123").request().get(String.class));
+
+        // Sent HTTP PUT request to update customer info
+        System.out.println("\n");
+        System.out.println("Sent HTTP PUT request to update customer info");
+        MyClient myClient = new MyClient();
+        String inputFile = myClient.getClass().getResource("/update_customer.xml").getFile();
+        inputFile = readStringFromFile(inputFile);
+        System.out.println("\nInputFile: " + inputFile);
+
+        Response postResponse = customers.request(MediaType.APPLICATION_XML_TYPE)
+                                        .post(Entity.entity(inputFile, MediaType.APPLICATION_XML_TYPE));
+        System.out.println("Response status code: " + postResponse.getStatus());
+        System.out.println("Response body: ");
+        System.out.println(postResponse.readEntity(String.class));
+
+        // Sent HTTP POST request to add customer
+        System.out.println("\n");
+        System.out.println("Sent HTTP POST request to add customer");
+        inputFile = myClient.getClass().getResource("/add_customer.xml").getFile();
+        inputFile = readStringFromFile(inputFile);
+        System.out.println("\nInputFile: " + inputFile);
+
+        postResponse = customers.request(MediaType.APPLICATION_XML_TYPE)
+                                .post(Entity.entity(inputFile, MediaType.APPLICATION_XML_TYPE));
+        System.out.println("Response status code: " + postResponse.getStatus());
+        System.out.println("Response body: ");
+        System.out.println(postResponse.readEntity(String.class));
+
+        System.out.println("\n");
+        System.exit(0);
+    }
+
+    private static String readStringFromFile(String filename) throws Exception {
+        String contents = "";
+        BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                contents += line;
+            }
+        } finally {
+        }
+        return contents;
+    }
+}
